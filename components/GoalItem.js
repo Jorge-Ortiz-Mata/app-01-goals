50c7794ea1515e9775ab36beabc227f069984580
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-11-23 17:22:13 +0000

    Adding Riple effect.

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View, Pressable } from 'react-native';
 const GoalInput = (props) => {
 
   return (
-    <Pressable onPress={props.deleteGoalT.bind(this, props.item.id)}>
+    <Pressable
+      android_ripple={{color: 'gray'}} // This is only for android.
+      style={(pressData) => pressData.pressed && styles.pressedItem} // This is for Both, apple and android.
+      onPress={props.deleteGoalT.bind(this, props.item.id)}>
       <View style={styles.goalContainer}>
         <Text style={styles.goalTitle}>{props.item.text}</Text>
       </View>
@@ -19,7 +22,10 @@ const styles = StyleSheet.create({
     marginHorizontal: 5,
     borderRadius: 10,
     padding: 10,
-    backgroundColor: '#C7BCA1'
+    backgroundColor: '#ffffff'
+  },
+  pressedItem: {
+    backgroundColor: '#ff0000'
   },
   goalTitle: {
     color: '#000',
